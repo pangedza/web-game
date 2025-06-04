f0361d1ea2762db4c02090c479e9b20aa8b83432
pangedza <fatal47887@gmail.com>
2025-06-04 21:39:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,1 @@
-rom telegram import Bot, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
-TOKEN = ''
-CHAT_ID = ''  # узнай в @userinfobot
-
-bot = Bot(token=TOKEN)
-1
-keyboard = [
-    [KeyboardButton(text="🎮 Играть в WOK", web_app=WebAppInfo(url="https://web-game1.vercel.app"))]
-]
-
-markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-
-bot.send_message(
-    chat_id=CHAT_ID,
-    text="Запусти игру прямо здесь в Telegram!",
-    reply_markup=markup
-)
